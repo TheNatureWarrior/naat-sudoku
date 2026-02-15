42bb1f95fdac407318235ce97836dda9b8a528f6
Naat Ambrosino <89532227+TheNatureWarrior@users.noreply.github.com>
2026-02-14 19:17:23 -0500

    Fix up grid test
    Getting this test back to its proper state now that I've verified the grid init is actually working now.

--- tests/grid_tests/test_grid_basics.py
+++ tests/grid_tests/test_grid_basics.py
@@ -13,6 +13,7 @@ def assertCellsIdentical(actual, expected):
 
 def assertGridIntegrity(grid : Grid) -> None:
     all_cells = list(grid.cells(include_solved = True))
+    assert len(all_cells) == 81
     for row in range(9):
         for col in range(9):
             cell = grid[row][col]
@@ -43,33 +44,5 @@ def assertGridIntegrity(grid : Grid) -> None:
             assert cell in all_cells
 
 class TestGridFundamentals:
-    @pytest.mark.parametrize('grid', [Grid()])
-    def test_grid_fundamentals(self, grid):
-        for row in range(9):
-            for col in range(9):
-                cell = grid[row][col]
-                assert cell.row == row
-                assert cell.column == col
-                box = cell.box
-                box_cell = cell.box_cell
-                assertCellsIdentical(grid.row(row)[col], cell)
-                assertCellsIdentical(grid.column(col)[row], cell)
-                assertCellsIdentical(grid.box(box)[box_cell], cell)
-                assertCellsIdentical(grid.division('row', row)[col], cell)
-                assertCellsIdentical(grid.division('column', col)[row], cell)
-                assertCellsIdentical(grid.division('box', box)[box_cell], cell)
-                strip_found = False
-                for _cell in grid.strip(cell.strip):
-                    if _cell == cell:
-                        assertCellsIdentical(_cell, cell)
-                        strip_found = True
-                        break
-                assert strip_found
-                chute_found = False
-                for _cell in grid.chute(cell.chute):
-                    if _cell == cell:
-                        assertCellsIdentical(_cell, cell)
-                        chute_found = True
-                        break
-                assert chute_found
-
+    def test_grid_init(self):
+        assertGridIntegrity(Grid())
